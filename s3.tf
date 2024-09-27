82b6a5e7d8e3e239ab30cae223cead79106b036a
Kaustubh Poshiya <kaustubh.posiya@techholding.co>
2024-09-27 12:28:10 +0530

    feat: resource to to create object locally

--- s3.tf
+++ s3.tf
@@ -9,5 +9,14 @@ resource "aws_s3_object" "object" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("${path.cwd}/${path.module}/s3/index.html")
+  # etag = filemd5("${path.cwd}/${path.module}/s3/index.html")
+
+  depends_on = [ local_file.default_object ]
+}
+
+resource "local_file" "default_object" {
+  count = var.put_default_root_object ? 1 : 0
+
+  content  = ""
+  filename = "${path.cwd}/${path.module}/s3/index.html"
 }
